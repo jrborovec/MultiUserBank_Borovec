fed34f4e1461da672add226a5af0c9326564eee8
jrborovec <167913176+jrborovec@users.noreply.github.com>
2024-05-19 14:46:10 -0700

    Add files via upload

---
+++ MultiUserBank_Borovec/Borovec_OOPBank/Bank.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borovec_OOPBank
+{
+    public class Bank
+    {
+        private int _bankbalance;
+        private string[] username = { "jlennon", "pmccartney", "gharrison", "rstarr" };
+        private string[] password = { "johnny", "pauly", "georgy", "ringoy" };
+        private int[] _acctbalance = { 1250, 2500, 3000, 1001 };
+        private int currentUser;
+
+
+        public Bank(int balance)
+        {
+            _bankbalance = balance;
+        }
+
+        public int BankBalance() { return _bankbalance; }
+        public string Username(int index) { return username[index]; }
+        public string Password(int index) { return password[index]; }
+        public int Balance(int index) { return _acctbalance[index]; }
+
+        public bool Login(string _username, string _password)
+        {
+            for (int i = 0; i < username.Length; i++)
+            {
+                if (username[i] == _username) 
+                { 
+                    if (password[i] == _password)
+                    {
+                        currentUser = i;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        //deposit
+        public void Deposit(int deposit)
+        {
+            _bankbalance += deposit;
+            _acctbalance[currentUser] += deposit;
+            Console.WriteLine("Deposit of $" + deposit + " successful...\n");
+            CheckBalance();
+        }
+        //withdrawal
+        public void Withdrawal(int withdrawal)
+        {
+            if (withdrawal > 500)
+            {
+                Console.WriteLine("Withdrawals are limited to $500 or less, your withdrawal will be adjusted.");
+                withdrawal = 500;            
+            }
+            if (withdrawal <= _acctbalance[currentUser])
+            {
+                Console.WriteLine("Withdrawal of $" + withdrawal + " successful...\n");
+                _acctbalance[currentUser] -= withdrawal;
+                _bankbalance -= withdrawal;
+            }
+            else
+            {
+                Console.WriteLine("Account Overdraw. $" + _acctbalance[currentUser] + " has been withdrawn instead.\n");
+                _bankbalance -= _acctbalance[currentUser];
+                _acctbalance[currentUser] = 0;
+
+            }
+            CheckBalance();
+        }
+
+        //check balance
+        public void CheckBalance()
+        {
+            Console.WriteLine("Current Balance: $" + _acctbalance[currentUser] + "\n");
+        }
+    }
+
+}
---
+++ MultiUserBank_Borovec/Borovec_OOPBank/Program.cs
@@ -0,0 +1,82 @@
+﻿//Written by Jonathan Borovec
+namespace Borovec_OOPBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank Bank = new Bank(10000);
+            bool isRunning = true;
+            bool loggedIn = false;
+
+            while (isRunning) {
+                int currentUser;
+                Console.WriteLine("------ Login Menu -----");
+                Console.WriteLine("Bank Balance: $" + Bank.BankBalance());
+                Console.WriteLine("1. Login");
+                Console.WriteLine("0. Exit");
+                int choice1 = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice1) 
+                {
+                    case 1:
+                        Console.WriteLine("Please enter your username:");
+                        string username = Console.ReadLine();
+                        Console.WriteLine("Please enter your password:");
+                        string password = Console.ReadLine();
+
+                        if (Bank.Login(username, password) == false)
+                        {
+                            Console.WriteLine("Login attempt failed, please check your login information.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Login successful!");
+                            loggedIn = true;
+                            break;
+                        }
+                    case 0:
+                        Console.WriteLine("Exiting...");
+                        isRunning = false;
+                        break;
+                }
+                
+                while (loggedIn)
+                {
+                Console.WriteLine("----- Main Menu -----");
+                Console.WriteLine("1. Make a Deposit");
+                Console.WriteLine("2. Make a Withdrawal");
+                Console.WriteLine("3. Check Current Balance");
+                Console.WriteLine("0. Exit");
+                int choice2 = Convert.ToInt32(Console.ReadLine());
+                    switch (choice2)
+                    {
+                        case 1:
+                            Console.WriteLine("How much would you like to deposit?");
+                            int deposit = Convert.ToInt32(Console.ReadLine());
+                            Bank.Deposit(deposit);
+                            break;
+                        case 2:
+                            Console.WriteLine("How much would you like to withdraw?");
+                            int withdrawal = Convert.ToInt32(Console.ReadLine());
+                            Bank.Withdrawal(withdrawal);
+                            break;
+                        case 3:
+                            Bank.CheckBalance();
+                            break;
+                        case 0:
+                            Console.WriteLine("Logging out...");
+                            loggedIn = false;
+                            break;
+                        default:
+                            Console.WriteLine("Please enter a valid selection.");
+                            break;
+
+
+                    }
+                }
+            }
+        }
+    }
+}
